b55f90ea64caeb0b05e62a7572aaf5621b28dc02
MarceloSebastian <na.marcelo.sebastian@gmail.com>
2021-03-08 16:20:36 -0500

    Home Page Done

--- src/StyledComponents.js
+++ src/StyledComponents.js
@@ -8,3 +8,51 @@ export const LogoImage = styled.figure`
   }
 `
 
+export const WrapperHome = styled.div`
+  min-height: calc(100vh - 84px);
+  `
+
+export const Hero = styled.div`
+  min-height: calc(100vh - 84px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 100px 0;
+  
+  figure {
+    width: 590px;
+    margin-top: -140px;
+  }
+
+  img {
+    width: 100%;
+  }
+
+  p {
+    color: #f9f9f9;
+    font-size: 17px;
+    letter-spacing: 1px;
+    text-align: center;
+    font-weight: 500;
+  }
+
+  a {
+    margin: 10px 0;
+    padding: 10px 15px;
+    color: #f9f9f9;
+    font-weight: 700;
+    font-size: 12px;
+    letter-spacing: 2px;
+    border: 1px solid rgb(59 212 255);
+    text-decoration: none;
+    text-transform: uppercase;
+    background-color: rgba(0, 0, 0, 0.5);
+    margin-top: 25px;
+  }
+
+  img path{
+    fill: red;
+  }
+
+`
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,19 @@
+import { Link } from 'react-router-dom';
+import title from '../static/images/title.png';
+import { WrapperHome, Hero } from '../StyledComponents';
+
+
 const HomePage = () => {
   return ( 
-    <h1>Home Page</h1>
+    <WrapperHome className='imagebg'>
+      <Hero>
+        <figure>
+          <img src={title} alt="title"/>
+          <p>League of Legends is a team-based strategy game where two teams of five powerful champions face off to destroy the other’s base. Choose from over 140 champions to make epic plays, secure kills, and take down towers as you battle your way to victory.</p>
+        </figure>
+        <Link to='/champions'>Champions List</Link>
+      </Hero>
+    </WrapperHome>
    );
 }
  
